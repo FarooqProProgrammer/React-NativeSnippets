08adee0367d02c6331a6da646112d4e6daf20ad1
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-02-11 17:22:53 +0500

    Create ImagePicker.js

---
+++ ImagePicker.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { View, Text, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+const ImagePickerExample = () => {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
+  );
+};
+
+export default ImagePickerExample;
